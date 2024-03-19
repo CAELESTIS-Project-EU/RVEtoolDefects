9af8d6b147ffe7de0f4ce34a66b85598656380e9
gguillam <gerard.guillamet@bsc.es>
2024-03-19 09:32:06 +0100

    Issue #main hotfix in line commented

--- RVE_mesher/src/main3D.py
+++ RVE_mesher/src/main3D.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     gmshBinFile = 'gmsh'
     #gmshBinFile = '/gpfs/projects/bsce81/gmsh/gmsh-4.11.1-Linux64/bin/gmsh'
     gmsh2alya = 'gmsh2alya.pl'
-    # gmshBinFile = '/gpfs/projects/bsce81/alya/builds/gmsh2alya.pl'
+    #gmsh2alya = '/gpfs/projects/bsce81/alya/builds/gmsh2alya.pl'
     
     # Run mesher
     mesher3D(case, gmshBinFile, gmsh2alya, dataPath, outputPath, h, c, nOfLevels, generateCohesiveElements)
